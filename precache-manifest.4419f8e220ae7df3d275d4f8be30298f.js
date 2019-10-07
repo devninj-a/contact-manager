1322a3b714a0ca4d7937a36dc1c907dcfd64bb0c
devninj-a <kxmg18@gmail.com>
2019-10-07 17:33:35 +0800

    Updates

---
+++ precache-manifest.4419f8e220ae7df3d275d4f8be30298f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c380c17e51163e541795e198efcd1571",
+    "url": "/contact-manager/index.html"
+  },
+  {
+    "revision": "47cedc7502a02b2823ea",
+    "url": "/contact-manager/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "bc578b30f38b8e9c4ae2",
+    "url": "/contact-manager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "47cedc7502a02b2823ea",
+    "url": "/contact-manager/static/js/2.9da6174d.chunk.js"
+  },
+  {
+    "revision": "bc578b30f38b8e9c4ae2",
+    "url": "/contact-manager/static/js/main.e5351e1c.chunk.js"
+  },
+  {
+    "revision": "f19a19bb196343f90d7a",
+    "url": "/contact-manager/static/js/runtime-main.3617d03a.js"
+  }
+]);
